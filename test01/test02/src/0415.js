489a9763040769a156540ac79eb253477f9c1c74
nol1214 <merylqls169@gmail.com>
2025-04-16 21:15:32 +0900

    Add files via upload

--- test01/test02/src/0415.js
+++ test01/test02/src/0415.js
@@ -14,7 +14,7 @@ rl.question("키를 입력하세요 (cm): ", function (input) {
         console.log("NO");
     }
     rl.close();
-});*/
+});
 
 const readline = require("readline");
 const rl = readline.createInterface({
@@ -43,3 +43,83 @@ rl.question("키가 몇 cm 인가요? ", function(input) {
     rider.checkEligibility();
     rl.close();
 });
+
+//18번 문제
+const readline = require("readline");
+
+class ScoreCalculator {
+    constructor(scores) {
+        this.scores = scores;
+    }
+
+    getAverage() {
+        let sum = this.scores.reduce((acc, val) => acc + val, 0);
+        let avg = Math.floor(sum / this.scores.length);
+        console.log(avg);
+    }
+}
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.question("입력 : ", function(input) {
+    let scores = input.trim().split(" ").map(Number);
+    let calculator = new ScoreCalculator(scores);
+    calculator.getAverage();
+    rl.close();
+});
+*/
+
+//25번
+const readline = require("readline");
+
+function getCircleArea(radius) {
+    return radius * radius * 3.14;
+}
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.question("반지름의 길이를 입력하세요: ", function(input) {
+    let radius = parseInt(input);
+    let area = getCircleArea(radius);
+    console.log("원의 넓이:", area);
+    rl.close();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
