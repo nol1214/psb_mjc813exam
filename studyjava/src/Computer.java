6b5b9876558db6e9f3dfb354254bba9dfee496c2
nol1214 <merylqls169@gmail.com>
2025-05-16 21:47:12 +0900

    5월 16일 문제수정
    기존 컴퓨터 한개로 구현한 것을 5개로 다시 수정

--- studyjava/src/Computer.java
+++ studyjava/src/Computer.java
@@ -1,59 +1,13 @@
 public class Computer {
         private String name;
         private String cpu;
-        private Integer ram; // GB
+        private Integer ram;
         private String storageType;
-        private Integer storageSize; // GB
+        private Integer storageSize;
         private String graphic;
         private Mouse mouse;
 
-        public Computer() {
-            this.name = "Computer";
-            this.cpu = null;
-            this.ram = null;
-            this.storageType = null;
-            this.storageSize = null;
-            this.graphic = null;
-            this.mouse = new Mouse();
-        }
-
-        public Computer(String name) {
-            this.name = name;
-            this.cpu = null;
-            this.ram = null;
-            this.storageType = null;
-            this.storageSize = null;
-            this.graphic = null;
-            this.mouse = new Mouse();
-        }
-        public Computer(String name, String cpu) {
-            this.name = name;
-            this.cpu = cpu;
-            this.ram = null;
-            this.storageType = null;
-            this.storageSize = null;
-            this.graphic = null;
-            this.mouse = new Mouse();
-        }
-        public Computer(String name, String cpu, int ram) {
-            this.name = name;
-            this.cpu = cpu;
-            this.ram = ram;
-            this.storageType = null;
-            this.storageSize = null;
-            this.graphic = null;
-            this.mouse = new Mouse();
-        }
-        public Computer(String name, String cpu, int ram, String storageType, int storageSize) {
-            this.name = name;
-            this.cpu = cpu;
-            this.ram = ram;
-            this.storageType = storageType;
-            this.storageSize = storageSize;
-            this.graphic = null;
-            this.mouse = new Mouse();
-        }
-        public Computer(String name, String cpu, int ram, String storageType, int storageSize, String graphic) {
+        public Computer(String name, String cpu, Integer ram, String storageType, Integer storageSize, String graphic) {
             this.name = name;
             this.cpu = cpu;
             this.ram = ram;
@@ -62,9 +16,6 @@ public class Computer {
             this.graphic = graphic;
             this.mouse = new Mouse();
         }
-
-
-
         public void powerOn() {
             System.out.println(name + " 켜졌습니다.");
         }
@@ -79,11 +30,11 @@ public class Computer {
             System.out.println("Graphic : " + (graphic != null ? graphic : "조립중입니다"));
             System.out.println();
         }
-        public void clickLeftAt200_400() {
+        public void clickLeftAt200x400() {
             mouse.moveMouse(200, 400);
             mouse.leftClick();
         }
-        public void clickRightAt600_800() {
+        public void clickRightAt600x800() {
             mouse.moveMouse(600, 800);
             mouse.rightClick();
         }
--- studyjava/src/Main.java
+++ studyjava/src/Main.java
@@ -2,11 +2,25 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Computer com = new Computer("Samsung NT530", "Gen9_i7", 32);
-        com.powerOn();
-        com.printSpec();
+        Computer[] computers = new Computer[5];
 
-        com.clickLeftAt200_400();
-        com.clickRightAt600_800();
+        computers[0] = new Computer("mjc813", "Gen9_i7", 16, "HDD", 500, "GTX_1660");
+        computers[1] = new Computer("Work", "Gen9_i9", 32, "SSD", 2000, "RTX_4080");
+        computers[2] = new Computer("Gaming", "Gen9_i5", 8, "SSD", 1000, "Intel_A770");
+        computers[3] = new Computer("Mini", "Gen9_i7", 4, "HDD", 1000, null);
+        computers[4] = new Computer("Machine", "Gen9_i9", 32, "SSD", 1000, "GTX_1660");
+
+        for (Computer c : computers) {
+            c.powerOn();
+        }
+
+        System.out.println("\n컴퓨터 스펙");
+        for (Computer c : computers) {
+            c.printSpec();
+        }
+
+        for (Computer c : computers) {
+            c.powerOff();
+        }
     }
 }
