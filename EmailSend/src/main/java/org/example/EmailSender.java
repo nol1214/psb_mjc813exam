ebf92774a44f2a2cd72880cfdf182a90c63f176f
nol1214 <merylqls169@gmail.com>
2025-05-27 21:12:34 +0900

    javamail api 사용

---
+++ EmailSend/src/main/java/org/example/EmailSender.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import java.util.Properties;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class EmailSender {
+    public static void main(String[] args) {
+        String host = "smtp.gmail.com";
+        final String user = "123123123@gmail.com";
+        final String password = "12312312312313";
+
+        String to = "123123123@gmail.com";
+        Properties props = new Properties();
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        Session session = Session.getInstance(props, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(user, password);
+            }
+        });
+
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(user));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            message.setSubject("아무튼 적당한 제목");
+            message.setText("이것이...자바다........");
+
+            Transport.send(message);
+            System.out.println("이메일 전송 성공");
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
